2c3b2651a8ab227b45278d0a2549eeda19315449
Andrei Horak <linkyndy@gmail.com>
2016-09-08 14:48:56 +0200

    Implement listing prime numbers whose reverse are also prime

--- common.hs
+++ common.hs
@@ -2,3 +2,13 @@ module Common where
 
 factorial :: Int -> Int
 factorial n = foldl (*) 1 [1..n]
+
+is_prime :: Integer -> Bool
+is_prime n = n > 1 && all (\x -> rem n x /= 0) xs
+  where xs = takeWhile (\y -> y^2 <= n) prime_numbers
+
+prime_numbers :: [Integer]
+prime_numbers = 2 : filter is_prime [3..]
+
+reverse_number :: Integer -> Integer
+reverse_number = read . reverse . show
