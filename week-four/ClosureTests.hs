5227c3b4487c1e0eea2148b66ec3ef7c394a9a15
Andrei Horak <linkyndy@gmail.com>
2016-10-02 11:35:13 +0200

    Add tests for transitiveClosure

--- week-four/ClosureTests.hs
+++ week-four/ClosureTests.hs
@@ -4,6 +4,7 @@ import Data.List
 import Test.QuickCheck
 import Common
 import SymmetricClosure
+import TransitiveClosure
 
 -- Check whether all pairs from both the relation and its inverse are in the
 -- generated symmetric closure
@@ -14,3 +15,14 @@ prop_symmetric r = and [elem p (symmetricClosure r) | p <- nub (r ++ (inverseRel
 -- the number of unique pairs from both the relation and its inverse
 prop_cardinality :: Rel Int -> Bool
 prop_cardinality r = length (symmetricClosure r) == length (nub (r ++ (inverseRelation r)))
+
+-- Check that pairs from the initial relation are present in the generated
+-- transitive closure
+prop_initial_pairs :: Rel Int -> Bool
+prop_initial_pairs r = and [elem p closure | p <- r]
+  where closure = transitiveClosure r
+
+-- Check whether the resulting relation is transitive
+prop_transitive :: Rel Int -> Bool
+prop_transitive r = and [elem (a,c) closure | (a,b) <- closure, (b',c) <- closure, b == b']
+  where closure = transitiveClosure r
